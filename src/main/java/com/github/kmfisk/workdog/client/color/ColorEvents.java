bb04ef73e7820c558eb9c7d83f17429c428ba477
Mnesikos <mnesikos@gmail.com>
2025-06-28 00:41:02 -0400

    Update ColorEvents.java

--- src/main/java/com/github/kmfisk/workdog/client/color/ColorEvents.java
+++ src/main/java/com/github/kmfisk/workdog/client/color/ColorEvents.java
@@ -10,14 +10,14 @@ import net.minecraftforge.client.event.RegisterColorHandlersEvent;
 public class ColorEvents {
     public static void registerColorHandlerBlocks(final RegisterColorHandlersEvent.Block event) {
         event.getBlockColors().register((state, reader, pos, color) -> reader != null && pos != null ? BiomeColors.getAverageWaterColor(reader, pos) : -1,
-                WorkDogBlocks.BOWLS.get(DyeColor.WHITE.getName()).get(), WorkDogBlocks.BOWLS.get(DyeColor.ORANGE.getName()).get(),
-                WorkDogBlocks.BOWLS.get(DyeColor.MAGENTA.getName()).get(), WorkDogBlocks.BOWLS.get(DyeColor.LIGHT_BLUE.getName()).get(),
-                WorkDogBlocks.BOWLS.get(DyeColor.YELLOW.getName()).get(), WorkDogBlocks.BOWLS.get(DyeColor.LIME.getName()).get(),
-                WorkDogBlocks.BOWLS.get(DyeColor.PINK.getName()).get(), WorkDogBlocks.BOWLS.get(DyeColor.GRAY.getName()).get(),
-                WorkDogBlocks.BOWLS.get(DyeColor.LIGHT_GRAY.getName()).get(), WorkDogBlocks.BOWLS.get(DyeColor.CYAN.getName()).get(),
-                WorkDogBlocks.BOWLS.get(DyeColor.PURPLE.getName()).get(), WorkDogBlocks.BOWLS.get(DyeColor.BLUE.getName()).get(),
-                WorkDogBlocks.BOWLS.get(DyeColor.BROWN.getName()).get(), WorkDogBlocks.BOWLS.get(DyeColor.GREEN.getName()).get(),
-                WorkDogBlocks.BOWLS.get(DyeColor.RED.getName()).get(), WorkDogBlocks.BOWLS.get(DyeColor.BLACK.getName()).get());
+                WorkDogBlocks.BOWLS.get(DyeColor.WHITE).get(), WorkDogBlocks.BOWLS.get(DyeColor.ORANGE).get(),
+                WorkDogBlocks.BOWLS.get(DyeColor.MAGENTA).get(), WorkDogBlocks.BOWLS.get(DyeColor.LIGHT_BLUE).get(),
+                WorkDogBlocks.BOWLS.get(DyeColor.YELLOW).get(), WorkDogBlocks.BOWLS.get(DyeColor.LIME).get(),
+                WorkDogBlocks.BOWLS.get(DyeColor.PINK).get(), WorkDogBlocks.BOWLS.get(DyeColor.GRAY).get(),
+                WorkDogBlocks.BOWLS.get(DyeColor.LIGHT_GRAY).get(), WorkDogBlocks.BOWLS.get(DyeColor.CYAN).get(),
+                WorkDogBlocks.BOWLS.get(DyeColor.PURPLE).get(), WorkDogBlocks.BOWLS.get(DyeColor.BLUE).get(),
+                WorkDogBlocks.BOWLS.get(DyeColor.BROWN).get(), WorkDogBlocks.BOWLS.get(DyeColor.GREEN).get(),
+                WorkDogBlocks.BOWLS.get(DyeColor.RED).get(), WorkDogBlocks.BOWLS.get(DyeColor.BLACK).get());
     }
 
     public static void registerColorHandlerItems(final RegisterColorHandlersEvent.Item event) {
