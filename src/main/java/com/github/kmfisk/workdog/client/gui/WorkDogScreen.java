45fd88183e88273483e8d47b29cea92c4f4e9348
Mnesikos <mnesikos@gmail.com>
2026-04-06 15:39:15 -0400

    hide saddlebag slot from gui
    chested animals don't have a slot for chests, they're just equipped by using the item on the animal!

--- src/main/java/com/github/kmfisk/workdog/client/gui/WorkDogScreen.java
+++ src/main/java/com/github/kmfisk/workdog/client/gui/WorkDogScreen.java
@@ -40,7 +40,7 @@ public class WorkDogScreen extends AbstractContainerScreen<WorkDogInventoryMenu>
             if (dog.canWearDogEquipmentType(AbstractInventoryAnimal.DogEquipmentType.HARNESS)) guiGraphics.blit(TEXTURE, i + 49, j + 95, 180, 205, 18, 18, 384, 256);
             if (dog.canWearDogEquipmentType(AbstractInventoryAnimal.DogEquipmentType.VEST)) guiGraphics.blit(TEXTURE, i + 49, j + 115, 198, 205, 18, 18, 384, 256);
             if (dog.canWearDogEquipmentType(AbstractInventoryAnimal.DogEquipmentType.MUZZLE)) guiGraphics.blit(TEXTURE, i + 49, j + 135, 216, 205, 18, 18, 384, 256);
-            if (dog.canEquipSaddlebag()) guiGraphics.blit(TEXTURE, i + 49, j + 155, 234, 205, 18, 18, 384, 256);
+//            if (dog.canEquipSaddlebag()) guiGraphics.blit(TEXTURE, i + 49, j + 155, 234, 205, 18, 18, 384, 256);
         }
     }
 }
