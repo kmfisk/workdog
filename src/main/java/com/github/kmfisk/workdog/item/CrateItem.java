f6ff59b543fe9cea2e5b36ad3e7584160aff59f9
Mnesikos <mnesikos@gmail.com>
2026-05-10 14:49:07 -0400

    cleanup CrateItem

--- src/main/java/com/github/kmfisk/workdog/item/CrateItem.java
+++ src/main/java/com/github/kmfisk/workdog/item/CrateItem.java
@@ -10,8 +10,10 @@ import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.TamableAnimal;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
@@ -20,48 +22,44 @@ import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 
 import javax.annotation.Nullable;
+import java.util.Arrays;
 import java.util.List;
 
 public class CrateItem extends Item {
+    private static final List<String> IGNORED_TAGS = Arrays.asList("Brain", "HurtByTimestamp", "FallFlying", "PortalCooldown", "FallDistance", "DeathTime", "HandDropChances", "Motion", "LeftHanded", "Air", "OnGround", "Rotation", "ArmorDropChances", "Pos", "Fire", "HurtTime", "NoGravity", "Passengers", "Leash");
+
     public CrateItem(Properties properties) {
         super(properties);
     }
 
     @Override
     public InteractionResult interactLivingEntity(ItemStack stack, Player player, LivingEntity target, InteractionHand hand) {
-        if (target instanceof WorkDogEntity) {
+        if (target instanceof WorkDogEntity dog) {
             if (stack.hasTag()) {
                 player.displayClientMessage(Component.translatable("chat.workdog.crate.full"), true);
                 return InteractionResult.PASS;
             }
 
-            WorkDogEntity dog = (WorkDogEntity) target;
             if (!dog.isTame() || dog.getOwner() == player || player.hasPermissions(2)) {
+                dog.revive();
                 if (player.level().isClientSide) return InteractionResult.SUCCESS;
+
                 ItemStack capturedEntityItem = caughtEntityItem(dog, player);
                 player.setItemInHand(hand, capturedEntityItem);
-                return InteractionResult.CONSUME;
+
             } else player.displayClientMessage(Component.translatable("chat.workdog.crate.not_your_dog"), true);
 
         } else
             player.displayClientMessage(Component.translatable("chat.workdog.crate.fail"), true);
 
-        return super.interactLivingEntity(stack, player, target, hand);
+        return InteractionResult.SUCCESS;
     }
 
     private ItemStack caughtEntityItem(WorkDogEntity dog, Player player) {
-        dog.stopRiding();
-        dog.ejectPassengers();
-        dog.revive();
-
         CompoundTag tags = new CompoundTag();
         dog.save(tags);
-
-        ResourceLocation key = EntityType.getKey(dog.getType());
-        tags.putString("id", key.toString());
+        IGNORED_TAGS.forEach((tags::remove));
         if (dog.isTame() && dog.isOwnedBy(player)) tags.putString("OwnerName", player.getName().getString());
-        if (dog.hasCustomName()) tags.putString("DisplayName", dog.getDisplayName().getString());
-
         dog.discard();
         player.displayClientMessage(Component.translatable("chat.workdog.crate.capture", dog.getDisplayName()), true);
 
@@ -81,40 +79,36 @@ public class CrateItem extends Item {
                 return InteractionResult.PASS;
             }
 
-            if (!level.isClientSide) {
-                BlockPos pos = new BlockPos(context.getClickedPos()).relative(context.getClickedFace());
-
-                CompoundTag tags = stack.getTag();
-                tags.remove("Passengers");
-                tags.remove("Leash");
-                tags.remove("OwnerName");
-                tags.remove("DisplayName");
-
-                LivingEntity entity = (LivingEntity) EntityType.loadEntityRecursive(tags, level, entity1 -> entity1);
-                if (entity != null) {
-                    entity.absMoveTo(pos.getX() + 0.5D, pos.getY(), pos.getZ() + 0.5D, context.getRotation(), 0);
-                    entity.setUUID(tags.getUUID("UUID"));
-                    level.addFreshEntity(entity);
-
-                    stack.shrink(1);
-                    player.setItemInHand(context.getHand(), new ItemStack(this));
-
-                    player.displayClientMessage(Component.translatable("chat.workdog.crate.release", entity.getDisplayName()), true);
+            BlockPos pos = new BlockPos(context.getClickedPos()).relative(context.getClickedFace());
+            CompoundTag tags = stack.getTag();
+            IGNORED_TAGS.forEach((tags::remove));
+            Entity entity = EntityType.loadEntityRecursive(tags, level, entity1 -> entity1);
+            if (entity != null) {
+                entity.absMoveTo(pos.getX() + 0.5D, pos.getY(), pos.getZ() + 0.5D, context.getRotation(), 0);
+                if (entity instanceof TamableAnimal tamableAnimal) {
+                    tamableAnimal.setOrderedToSit(tamableAnimal.isTame());
+                    tamableAnimal.setInSittingPose(tamableAnimal.isTame());
+                    if (tamableAnimal instanceof WorkDogEntity workDog)
+                        workDog.setMode(WorkDogEntity.Mode.fromOrdinal(tags.getInt("Mode")));
                 }
-            }
+                if (!level.isClientSide) level.addFreshEntity(entity);
 
-            return InteractionResult.sidedSuccess(level.isClientSide);
+                stack.shrink(1);
+                player.setItemInHand(context.getHand(), new ItemStack(this));
+
+                player.displayClientMessage(Component.translatable("chat.workdog.crate.release", entity.getDisplayName()), true);
+            }
         }
 
-        return super.useOn(context);
+        return InteractionResult.SUCCESS;
     }
 
     @Override
     public void appendHoverText(ItemStack stack, @Nullable Level level, List<Component> tooltip, TooltipFlag flag) {
         CompoundTag nbt = stack.getTag();
         if (nbt != null && nbt.contains("id")) {
-            if (nbt.contains("DisplayName"))
-                tooltip.add(Component.literal("\"" + nbt.getString("DisplayName") + "\"").withStyle(ChatFormatting.AQUA));
+            if (nbt.contains("CustomName", 8))
+                tooltip.add(Component.literal("\"" + Component.Serializer.fromJson(nbt.getString("CustomName")).getString() + "\"").withStyle(ChatFormatting.AQUA));
 
             MutableComponent entityId = Component.translatable(Util.makeDescriptionId("entity", new ResourceLocation(nbt.getString("id"))));
             MutableComponent gender = Component.translatable(nbt.getBoolean("Gender") ? "tooltip.workdog.crate.male" : "tooltip.workdog.crate.female");
